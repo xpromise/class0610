8715347c646817825377c4279f4bf91518451e18
xiongjian <webjsforyou@gmail.com>
2020-10-20 10:38:20 +0800

    feat: 完成ajax

--- 02.react_cli/src/App.jsx
+++ 02.react_cli/src/App.jsx
@@ -1,37 +1,70 @@
 import React, { Component } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 export default class App extends Component {
+  state = {
+    isLoading: false,
+    repo: {},
+  };
 
   async componentDidMount() {
     try {
-      const result = await axios({
-        url: '/',
-        /*
-          GET    查
-          POST   增
-          PUT    改（全部修改）
-          PATCH  改（局部修改）
-          DELETE
-        */
-        method: 'GET', 
-      })
+      // 发送请求之前，更新loading
+      this.setState({
+        isLoading: true,
+      });
 
-      console.log(result);
+      // const result = await axios({
+      //   url: "/repo",
+      //   /*
+      //     GET    查
+      //     POST   增
+      //     PUT    改（全部修改）
+      //     PATCH  改（局部修改）
+      //     DELETE
+      //   */
+      //   method: "GET",
+      // });
+
+      // if (result.data.code === 0) {
+      //   this.setState({
+      //     repo: result.data.data.items[0],
+      //   });
+      // }
+
+      const result = await fetch('/repo').then(res => res.json())
+
+      // console.log(result);
+      if (result.code === 0) {
+        this.setState({
+          repo: result.data.items[0],
+        });
+      }
+
+      this.setState({
+        isLoading: false,
+      });
       //  .then()
       //  .catch()
     } catch (e) {
       // 处理错误
       console.log(e);
     }
-
   }
 
   render() {
+    console.log("render");
+    const { isLoading, repo } = this.state;
+
     return (
       <div>
-        <h1>loading...</h1>
-        <h1>most star repo is xxx</h1>
+        {isLoading ? (
+          <h1>loading...</h1>
+        ) : (
+          <h1>
+            most star repo is <a href={repo.html_url}>{repo.name}</a>
+          </h1>
+        )}
       </div>
     );
   }
