1836077225228d6fb0aa0b0048dc2d285b1c65fa
xiongjian <webjsforyou@gmail.com>
2020-10-20 16:03:18 +0800

    feat: 完成users练习

--- 02.react_cli/src/pages/List/index.jsx
+++ 02.react_cli/src/pages/List/index.jsx
@@ -1,18 +1,85 @@
 import React, { Component } from "react";
+import PubSub from "pubsub-js";
+import axios from "axios";
+// 接口文档：https://docs.github.com/en/free-pro-team@latest/rest/reference/search#search-users
+// 请求地址：https://api.github.com/search/users?q=yyx
 
 export default class List extends Component {
+  state = {
+    users: [], // 用户数据
+    isLoading: false, // 请求中
+    isFirstView: true, // 是否显示enter xxx
+  };
+
+  componentDidMount() {
+    this.token = PubSub.subscribe("search", async (msg, searchName) => {
+      try {
+        // console.log(msg, searchName);
+        // 在发送请求之前，切换loading
+        this.setState({
+          isFirstView: false,
+          isLoading: true,
+        });
+        // 发送请求，请求github users数据
+        const result = await axios.get(
+          `https://api.github.com/search/users?q=${searchName}`
+        );
+        // avatar_url html_url id login
+        // console.log(result.data.items);
+        this.setState({
+          isLoading: false,
+          users: result.data.items.map((user) => {
+            return {
+              id: user.id,
+              html_url: user.html_url,
+              avatar_url: user.avatar_url,
+              login: user.login,
+            };
+          }),
+        });
+      } catch (e) {
+        alert("网络异常~请稍后试试~");
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    PubSub.unsubscribe(this.token);
+  }
+
   render() {
+    const { users, isLoading, isFirstView } = this.state;
+
+    if (isFirstView) {
+      return <h1>enter name to search</h1>;
+    }
+
+    if (isLoading) {
+      return <h1>loading...</h1>;
+    }
+
+    // console.log(users);
+
+    if (users.length === 0) {
+      return <h1>暂无该用户数据</h1>;
+    }
+
     return (
       <div className="row">
-        <div className="card">
-          <a href="https://github.com/aa" target="_blank">
-            <img
-              src="https://avatars1.githubusercontent.com/u/28438?v=4"
-              style={{ width: 100 }}
-            />
-          </a>
-          <p className="card-text">aa</p>
-        </div>
+        {users.map((user) => {
+          return (
+            <div className="card" key={user.id}>
+              <a href={user.html_url} target="_blank" rel="noopener noreferrer">
+                <img
+                  src={user.avatar_url}
+                  style={{ width: 100 }}
+                  alt="用户头像"
+                />
+              </a>
+              <p className="card-text">{user.login}</p>
+            </div>
+          );
+        })}
       </div>
     );
   }
--- 02.react_cli/src/pages/Search/index.jsx
+++ 02.react_cli/src/pages/Search/index.jsx
@@ -1,13 +1,37 @@
 import React, { Component } from "react";
+import PubSub from "pubsub-js";
 
 export default class Search extends Component {
+  state = {
+    searchName: "",
+  };
+
+  search = () => {
+    const { searchName } = this.state;
+    
+    if (!searchName) return;
+    // 发布消息：将searchName传给List组件
+    PubSub.publish("search", searchName);
+  };
+
+  updateSearchName = (e) => {
+    this.setState({
+      searchName: e.target.value.trim(),
+    });
+  };
+
   render() {
     return (
       <section className="jumbotron">
         <h3 className="jumbotron-heading">Search Github Users</h3>
         <div>
-          <input type="text" placeholder="enter the name you search" />
-          <button>Search</button>
+          <input
+            type="text"
+            value={this.state.searchName}
+            onChange={this.updateSearchName}
+            placeholder="enter the name you search"
+          />
+          <button onClick={this.search}>Search</button>
         </div>
       </section>
     );
