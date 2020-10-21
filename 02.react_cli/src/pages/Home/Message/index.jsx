7437802789933828870570bc3bef892a100e1421
xiongjian <webjsforyou@gmail.com>
2020-10-21 11:44:55 +0800

    feat: 嵌套路由

---
+++ 02.react_cli/src/pages/Home/Message/index.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Message extends Component {
+  render() {
+    return <div>Message...</div>;
+  }
+}
--- 02.react_cli/src/pages/Home/index.jsx
+++ 02.react_cli/src/pages/Home/index.jsx
@@ -1,7 +1,29 @@
 import React, { Component } from "react";
 
+import { NavLink, Route, Switch, Redirect } from "react-router-dom";
+
+import News from "./News";
+import Message from "./Message";
+
 export default class Home extends Component {
   render() {
-    return <div>Home...</div>;
+    return (
+      <>
+        <h1>Home...</h1>
+        <ul className="nav nav-tabs">
+          <li>
+            <NavLink to="/home/news">News</NavLink>
+          </li>
+          <li>
+            <NavLink to="/home/message">Message</NavLink>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/home/news" component={News} />
+          <Route path="/home/message" component={Message} />
+          <Redirect to="/home/news" />
+        </Switch>
+      </>
+    );
   }
 }
